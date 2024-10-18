fe226fd92a0e413923cbbd2db17cb668c3a39ddb
Oleksii Osadchyi <alexey.osadchiy@gmail.com>
2024-10-18 15:29:45 +0300

    Add LOG

--- src/main/java/com/agent/Agent.java
+++ src/main/java/com/agent/Agent.java
@@ -1,36 +1,58 @@
 package com.agent;
 
 
+import java.io.IOException;
 import java.lang.instrument.Instrumentation;
+import java.util.logging.*;
+
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.matcher.ElementMatchers;
 
-import static net.bytebuddy.matcher.ElementMatchers.*;
 
 public class Agent {
 
-    public static void premain(String arguments, Instrumentation instrumentation) {
+    public static final Logger LOGGER = Logger.getAnonymousLogger();
+    private static final String PATH = "com.example";
 
-        System.out.println("Agent for get all methods");
+    private static class MyCustomFormatter extends Formatter {
+        @Override
+        public String format(LogRecord record) {
+            return record.getMessage() + "\n";
+        }
+    }
+
+    static {
+        try {
+            FileHandler fileHandler = new FileHandler("/tmp/coverage.log");
+            fileHandler.setFormatter(new MyCustomFormatter());
+
+            LOGGER.addHandler(fileHandler);
+            LOGGER.setUseParentHandlers(false);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void premain(String arguments, Instrumentation instrumentation) {
+        System.out.println("Agent is running");
 
         new AgentBuilder.Default()
                 .with(new AgentBuilder.InitializationStrategy.SelfInjection.Eager())
-                .type((ElementMatchers.nameStartsWith("com.example")))
-                .transform((builder, typeDescription, classLoader, javaModule, protectionDomain) ->
-                        builder
-                                .method(ElementMatchers.any())
-                                .intercept(Advice.to(AllMethod.class)))
+                .type(ElementMatchers.nameStartsWith(PATH))
+                .transform((builder, typeDescription, classLoader, javaModule, protectionDomain) -> {
+                    typeDescription.getDeclaredMethods()
+                            .forEach(method -> {
+                                if(method.isMethod()) {
+                                    LOGGER.info(typeDescription.getCanonicalName() + " - " + method.getName());
+                                }
+                            });
+                    LOGGER.info("--------------------------------");
+                    return builder
+                            .method(ElementMatchers.any())
+                            .intercept(Advice.to(AllMethod.class));}
+                )
                 .installOn(instrumentation);
+
     }
-}
-
-class AllMethod {
-
-    @Advice.OnMethodExit
-    static void getAllMethods(@Advice.Origin String method) throws Exception {
-
-        System.out.println("Trace --- " + method);
-    }
-
 }
